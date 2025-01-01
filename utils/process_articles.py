7b71cb884411809faa67b5a84d0ac0bbabe14842
hieuctwo <155509207+hieuctwo@users.noreply.github.com>
2025-01-01 14:19:06 +0700

    Update process_articles.py

--- utils/process_articles.py
+++ utils/process_articles.py
@@ -3,12 +3,11 @@ import shutil
 from urllib.parse import urlparse
 from constants import ARTICLE_URL
 
-def download_article(udemy, article, download_folder_path, title_of_output_article, task_id, progress):
-
-    progress.update(task_id,  description=f"Downloading Article {title_of_output_article}", completed=0)
+def download_article(udemy, article, download_folder_path, title_of_output_article, task_id, progress, portal_name):
+    progress.update(task_id, description=f"Downloading Article {title_of_output_article}", completed=0)
 
     article_filename = f"{title_of_output_article}.html"
-    article_response = udemy.request(ARTICLE_URL.format(article_id=article['id'])).json()
+    article_response = udemy.request(ARTICLE_URL.format(portal_name=portal_name, article_id=article['id'])).json()
 
     with open(os.path.join(os.path.dirname(download_folder_path), article_filename), 'w', encoding='utf-8', errors='replace') as file:
         file.write(article_response['body'])
