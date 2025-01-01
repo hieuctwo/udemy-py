601a19e877deba30d0e2a4d8f84a5c0659cd7758
hieuctwo <155509207+hieuctwo@users.noreply.github.com>
2025-01-01 14:18:02 +0700

    Update process_mp4.py

--- utils/process_mp4.py
+++ utils/process_mp4.py
@@ -3,7 +3,7 @@ import shutil
 import requests
 from constants import remove_emojis_and_binary
 
-def download_mp4(mp4_file_url, download_folder_path, title_of_output_mp4, task_id, progress):
+def download_mp4(mp4_file_url, download_folder_path, title_of_output_mp4, task_id, progress, portal_name):
     progress.update(task_id,  description=f"Downloading Video {remove_emojis_and_binary(title_of_output_mp4)}", completed=0)
     output_path = os.path.dirname(download_folder_path)
     
